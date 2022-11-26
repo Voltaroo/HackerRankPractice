2b721af15ada725c24bef2a439b9d08548afe76e
Voltrifrodec <69749982+Voltaroo@users.noreply.github.com>
2022-11-26 22:26:54 +0100

    Solve Me First

---
+++ Alghoritms-Python/solve-me-first.py
@@ -0,0 +1,10 @@
+# Problem: https://www.hackerrank.com/challenges/solve-me-first/problem
+# Points: 1.0
+
+def solveMeFirst(a,b):
+	return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
