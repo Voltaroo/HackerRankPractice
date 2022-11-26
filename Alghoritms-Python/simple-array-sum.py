ab2e93ff71955237951b9dfbf17e52e45b3ac8d9
Voltrifrodec <69749982+Voltaroo@users.noreply.github.com>
2022-11-26 22:29:19 +0100

    Simple Array Sum

---
+++ Alghoritms-Python/simple-array-sum.py
@@ -0,0 +1,34 @@
+# Problem: https://www.hackerrank.com/challenges/simple-array-sum/problem
+# Points: 10.00
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    sum = 0
+    for a in ar:
+        sum += a
+    return int(sum)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
