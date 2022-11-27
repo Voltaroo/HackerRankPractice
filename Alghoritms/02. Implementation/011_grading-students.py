4713ed9ad936c5188d05025ff02dae8b14ee02d2
V0ltaroo <nendorans@gmail.com>
2022-11-27 11:28:20 +0100

    Redundant Code Delete

--- Alghoritms/02. Implementation/011_grading-students.py
+++ Alghoritms/02. Implementation/011_grading-students.py
@@ -1,23 +1,9 @@
 # Problem: https://www.hackerrank.com/challenges/grading/problem
 # Points: 10.00
 
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'gradingStudents' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY grades as parameter.
-#
 
 def gradingStudents(grades):
-    tmp = list(map(int,grades))
+    tmp = list(map(int, grades))
     
     for i in range(len(tmp)):
         next_closest = (tmp[i] // 5) * 5 + 5
@@ -26,22 +12,3 @@ def gradingStudents(grades):
             tmp[i] = next_closest
     
     return tmp
-    
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    grades_count = int(input().strip())
-
-    grades = []
-
-    for _ in range(grades_count):
-        grades_item = int(input().strip())
-        grades.append(grades_item)
-
-    result = gradingStudents(grades)
-
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
--- Alghoritms/02. Implementation/012_apple-and-orange.py
+++ Alghoritms/02. Implementation/012_apple-and-orange.py
@@ -1,25 +1,6 @@
 # Problem: https://www.hackerrank.com/challenges/apple-and-orange/problem
 # Points: 10.00
 
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'countApplesAndOranges' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER s
-#  2. INTEGER t
-#  3. INTEGER a
-#  4. INTEGER b
-#  5. INTEGER_ARRAY apples
-#  6. INTEGER_ARRAY oranges
-#
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     
@@ -39,30 +20,3 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
         # print(landing_pos_b[i]) if s <= landing_pos_b[i] <= t else print('X')
     print(res_x)
     print(res_y)
-    
-    
-
-if __name__ == '__main__':
-    first_multiple_input = input().rstrip().split()
-
-    s = int(first_multiple_input[0])
-
-    t = int(first_multiple_input[1])
-
-    second_multiple_input = input().rstrip().split()
-
-    a = int(second_multiple_input[0])
-
-    b = int(second_multiple_input[1])
-
-    third_multiple_input = input().rstrip().split()
-
-    m = int(third_multiple_input[0])
-
-    n = int(third_multiple_input[1])
-
-    apples = list(map(int, input().rstrip().split()))
-
-    oranges = list(map(int, input().rstrip().split()))
-
-    countApplesAndOranges(s, t, a, b, apples, oranges)
