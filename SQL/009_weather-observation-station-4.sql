e4b47bfa321aa717efecdc75a17a41a080c6544b
V0ltaroo <nendorans@gmail.com>
2023-02-18 22:48:39 +0100

    New Completed Challenges Add

---
+++ SQL/009_weather-observation-station-4.sql
@@ -0,0 +1,3 @@
+-- Basic Select - Weather Observation Station 4
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION; 
---
+++ SQL/011_weather-observation-station-6.sql
@@ -0,0 +1,8 @@
+-- Basic Select - Weather Observation Station 6
+
+SELECT CITY FROM STATION
+WHERE CITY LIKE 'A%'
+   OR CITY LIKE 'E%' 
+   OR CITY LIKE 'I%' 
+   OR CITY LIKE 'O%' 
+   OR CITY LIKE 'U%'; 
